81e267adb5a991164fdb7045b154d18efc80f9a5
minh8250vn <minh8250vn@gmail.com>
2021-01-13 15:06:31 +0700

    cửa sổ Splash và cửa sổ main

---
+++ app/src/main/java/com/example/ltandroid/SplashActivity.java
@@ -0,0 +1,46 @@
+package com.example.ltandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.graphics.drawable.Animatable;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import static java.lang.Thread.sleep;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private TextView appName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        appName = findViewById(R.id.appName);
+
+        Typeface typeface = ResourcesCompat.getFont( this,R.font.pacifico );
+        appName.setTypeface(typeface);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.animation);
+        appName.setAnimation(animation);
+
+        new Thread(){
+            public void run(){
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }.start();
+
+    }
+}
